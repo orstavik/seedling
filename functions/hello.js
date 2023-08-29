378d05ce9ca4802815dea6c2a193cf76da2ff5f8
orstavik <orstavik77@gmail.com>
2023-08-29 12:29:54 +0200

    testing2

---
+++ functions/hello.js
@@ -0,0 +1,6 @@
+console.log("global context: sunshine on the server");
+
+export function onRequest(context) {
+  console.log("request context: sunshine on the server");
+  return new Response("Hello, world!")
+}
